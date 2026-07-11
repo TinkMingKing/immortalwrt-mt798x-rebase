7fddfe4479ca7b28334c1694b77ac1d4fb790f65
TinkMingKing <294248306@qq.com>
2026-07-11 17:59:16 +0800

    update

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1004,9 +1004,17 @@ define Device/cmcc_xr30-nand-ubootmod
   PAGESIZE := 2048
   IMAGE_SIZE := 116736k
   KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  UBOOTENV_IN_UBI := 1
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := -recovery.itb
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+  fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGE/sysupgrade.itb := append-kernel | \
+  fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | append-metadata
+  ARTIFACTS := preloader.bin bl31-uboot.fip
+  ARTIFACT/preloader.bin := mt7981-bl2 spim-nand-ddr4
+  ARTIFACT/bl31-uboot.fip := mt7981-bl31-uboot cmcc_xr30
 endef
 TARGET_DEVICES += cmcc_xr30-nand-ubootmod
 
