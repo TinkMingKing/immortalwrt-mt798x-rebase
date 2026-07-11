31a7d700c86b874c6d996b4d4c1baf27df0133bb
TinkMingKing <294248306@qq.com>
2026-07-11 17:07:19 +0800

    update

--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -8,6 +8,25 @@ define Device/clx_s20p
 endef
 TARGET_DEVICES += clx_s20p
 
+define Device/cmcc_xr30-nand-mtkuboot
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := XR30 NAND
+  DEVICE_VARIANT := ((MTK U-Boot layout))
+  DEVICE_DTS := mt7981b-cmcc-xr30-nand-mtkuboot
+  DEVICE_DTS_DIR := ../dts-ext
+  DEVICE_PACKAGES := kmod-usb3 automount f2fsck mkf2fs
+  SUPPORTED_DEVICES := cmcc,xr30-mtkuboot
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 116736k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cmcc_xr30-nand-mtkuboot
+
 define Device/cudy_tr3000-v1-mtkuboot
   DEVICE_VENDOR := Cudy
   DEVICE_MODEL := TR3000
@@ -24,25 +43,6 @@ define Device/cudy_tr3000-v1-mtkuboot
   DEVICE_PACKAGES := kmod-usb3 automount
 endef
 TARGET_DEVICES += cudy_tr3000-v1-mtkuboot
-
-define Device/cmcc_xr30
-  DEVICE_VENDOR := CMCC
-  DEVICE_MODEL := XR30
-  DEVICE_DTS := mt7981b-cmcc-xr30
-  DEVICE_DTS_DIR := ../dts-ext
-  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 \
-  automount f2fsck mkf2fs
-  SUPPORTED_DEVICES := cmcc,xr30
-  UBINIZE_OPTS := -E 5
-  BLOCKSIZE := 128k
-  PAGESIZE := 2048
-  IMAGE_SIZE := 116736k
-  KERNEL_IN_UBI := 1
-  IMAGES += factory.bin
-  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
-  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-TARGET_DEVICES += cmcc_xr30
 
 define Device/h3c_magic-nx30-pro-mtkuboot
   DEVICE_VENDOR := H3C
